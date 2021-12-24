b13fb10f9f4fe1e2bb67eeef4453aa2b037a1d87
Swatantra Singh <68724074+swatantra-coder@users.noreply.github.com>
2021-12-24 12:33:28 +0530

    Create MaximumDepth.cpp

---
+++ BST/MaximumDepth.cpp
@@ -0,0 +1,12 @@
+// 104. Maximum Depth of Binary Tree
+
+int maxDepth(TreeNode* root) 
+    {
+       if(root==NULL)
+           return 0;
+        
+        int l=maxDepth(root->left);
+        int r=maxDepth(root->right);
+    
+        return 1+max(l,r);
+    }
